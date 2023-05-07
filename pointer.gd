ca9cfa97387b8d30f90eb1d91f30e8638f732825
adsuttman <adsuttman@gmail.com>
2023-05-07 09:37:37 -0400

    fixed double shoving

--- pointer.gd
+++ pointer.gd
@@ -7,19 +7,26 @@ extends Node2D
 @export var positive_color: Color
 @export var negative_color: Color
 
+var shove_delay: int = 0
+
 func _ready() -> void:
 	indicator.self_modulate = negative_color
 
 func _physics_process(delta: float) -> void:
 	var mouse_position = get_viewport().get_mouse_position()
 	position = mouse_position
+	shove_delay = clampi(shove_delay - 1,0,15)
+#	if shove_delay != 0:
+#		print(shove_delay)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if shove_area.overlaps_body(ball):
-			var force = position.direction_to(ball.position) * shove_strength
-			ball.apply_central_impulse(force)
-#			print("shoved")
+		if event.button_index == MouseButton.MOUSE_BUTTON_LEFT and shove_delay == 0:
+			if shove_area.overlaps_body(ball):
+				var force = position.direction_to(ball.position) * shove_strength
+				ball.apply_central_impulse(force)
+#				print("shoved")
+				shove_delay  = 15
 
 
 func _on_shove_area_body_entered(body: Node2D) -> void:
