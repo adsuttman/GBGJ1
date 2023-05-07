cd212f94cb8b334222516af411d45fc9fe64b235
adsuttman <adsuttman@gmail.com>
2023-05-07 14:28:07 -0400

    added retry and return to main menu buttons to game over screen

--- game.gd
+++ game.gd
@@ -11,8 +11,17 @@ func _physics_process(delta: float) -> void:
 
 func display_game_over(score: String):
 	$GameOver/GameOverText/Score.text = "Your score was: " + score
+	$Pointer.hide()
 	$GameOver.show()
 
 
 func _on_ball_unfrozen() -> void:
 	game_started.emit()
+
+
+func _on_retry_pressed() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_return_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
