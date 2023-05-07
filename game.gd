b982559a289a0102ce2ac04d4033094d5b840aa6
adsuttman <adsuttman@gmail.com>
2023-05-07 14:08:11 -0400

    added game over screen

--- game.gd
+++ game.gd
@@ -7,3 +7,7 @@ signal ball_touched_floor
 func _physics_process(delta: float) -> void:
 	if ball.position.y > 712:
 		ball_touched_floor.emit()
+
+func display_game_over(score: String):
+	$GameOver/GameOverText/Score.text = "Your score was: " + score
+	$GameOver.show()
--- timer_mode.gd
+++ timer_mode.gd
@@ -3,6 +3,7 @@ extends Node2D
 var time: int = 0
 @onready var timer = $Timer
 @onready var label = $UI/Time
+@onready var game = $Game
 
 func _ready() -> void:
 	start_timer()
@@ -13,3 +14,8 @@ func start_timer():
 func _on_timer_timeout() -> void:
 	time += 1
 	label.text = "\n" + String.num(time) + " seconds"
+
+
+func _on_game_ball_touched_floor() -> void:
+	timer.stop()
+	game.display_game_over(String.num(time) + " seconds")
